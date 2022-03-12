b9d434ddbe9309df37b3dfa49e156b4749b711cb
Thana Sithisombath <sithisombath@cpp.edu>
2022-03-11 18:05:58 -0800

    =Card Effect Builder.

--- Assets/Scripts/Target/Target.cs
+++ Assets/Scripts/Target/Target.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public abstract class Target : ScriptableObject
 {
     public abstract IEnumerator Targeting(Character self, System.Action<List<Character>> doEffect);
